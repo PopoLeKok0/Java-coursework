6c86bc6c10d2ecbd2017631d3650ec10ee322be5
PopoLeKok0 <104931732+PopoLeKok0@users.noreply.github.com>
2022-05-05 00:15:48 -0400

    Add files via upload

---
+++ Assignments/Assignment 4/Q3/StudentInfo.java
@@ -0,0 +1,28 @@
+/**
+ * Contains a method <code>void display()</code> that all the <code>main</code> methods
+ * call to display the student information. Fill the box with your personal
+ * information.
+ * 
+ * @author Marcel Turcotte (turcotte@eecs.uottawa.ca)
+ */
+
+public class StudentInfo {
+
+    /**
+     * Displays the student information: student name, id, section, etc for each
+     * member of the team.
+     */
+
+    public static void display() {
+
+	System.out.println("************************************************************");
+	System.out.println("*                                                          *");
+	System.out.println("*  Mouad Ben lahbib, 300259705, Classe A00, Lab A01        *");
+	System.out.println("*                                                          *");
+	System.out.println("*                                                          *");
+	System.out.println("************************************************************");
+	System.out.println();
+
+    }
+
+}
